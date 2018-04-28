51161c70ec0d46420165297874e73d31c6d33e01
Alcina <white_kaitou@hotmail.com>
2018-04-28 19:53:52 -0400

    add ReactFragment

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,7 +3,14 @@ import React from 'react';
 class StorePicker extends React.Component {
   //render + method
   render(){
-    return <p>Hi</p>
+    //return React.createElement('p', { className: 'hey', 'Heyoooo' });
+    return (
+      <React.Fragment>
+        <form className="store-selector">
+          <h2>Please Enter A Store</h2>
+        </form>
+      </React.Fragment>
+    )
   }
 }
 
