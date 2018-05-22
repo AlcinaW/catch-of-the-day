2575f187ce9d34ebabc430cb6fc0aa14500f5d78
Alcina <white_kaitou@hotmail.com>
2018-05-21 21:51:50 -0400

    fish form con't

--- src/components/EditFishForm.js
+++ src/components/EditFishForm.js
@@ -3,8 +3,15 @@ import React from "react";
 class EditFishForm extends React.Component {
   render() {
     return <div className="fish-edit">
-
-    </div>
+      <input type="text" name="name" />
+      <input type="text" name="price" />
+      <select type="text" name="status">
+        <option value="available">Fresh!</option>
+        <option value="unavailable">Sold out!</option>
+      </select>
+      <textarea name="desc"></textarea>
+      <input type="text" name="image" />
+     </div>
   }
 }
 
